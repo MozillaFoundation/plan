a911f6214549f557d7c41019cee526747d230dd1
David Ascher <da@mozillafoundation.org>
2014-12-08 15:50:13 -0800

    gah, remove multiple places to look env vars up

--- config/secrets.js
+++ config/secrets.js
@@ -10,8 +10,8 @@ module.exports = {
   sessionSecret: process.env.SESSION_SECRET || env.get('session_secret'),
 
   github: {
-    clientID: process.env.GITHUB_ID || env.get('GITHUB_CLIENTID'),
-    clientSecret: process.env.GITHUB_SECRET || env.get('GITHUB_CLIENTSECRET'),
+    clientID: env.get('GITHUB_CLIENTID'),
+    clientSecret: env.get('GITHUB_CLIENTSECRET'),
     callbackURL: '/auth/github/callback',
     passReqToCallback: true
   }
