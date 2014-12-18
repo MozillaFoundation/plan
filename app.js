bd35bd4c52fe833159e8b6640d57e1e51531b765
Andrew Sliwinski <andrewsliwinski@acm.org>
2014-12-17 20:34:28 -0800

    WIP

--- app.js
+++ app.js
@@ -1,127 +1,112 @@
 /**
- * Module dependencies.
+ * HTTP server for build.webmaker.org
+ *
+ * @package build
+ * @author  David Ascher <davida@mozillafoundation.org>
+ *          Andrew Sliwinski <a@mozillafoundation.org>
  */
 
 var express = require('express');
 var cookieParser = require('cookie-parser');
 var compress = require('compression');
-var session = require('express-session');
 var bodyParser = require('body-parser');
 var logger = require('morgan');
 var errorHandler = require('errorhandler');
-var csrf = require('lusca').csrf();
-var methodOverride = require('method-override');
 
-var _ = require('lodash');
-var MongoStore = require('connect-mongo')(session);
+var sessions = require('client-sessions');
+var lusca = require('lusca');
 var flash = require('express-flash');
 var path = require('path');
-var mongoose = require('mongoose');
-var passport = require('passport');
 var expressValidator = require('express-validator');
 var connectAssets = require('connect-assets');
 
 /**
  * Controllers (route handlers).
  */
-
 var simpleController = require('./controllers/simple');
 var userController = require('./controllers/user');
 var apiController = require('./controllers/api');
 var intakeController = require('./controllers/intake');
 
 /**
- * API keys and Passport configuration.
+ * Import API keys from environment
  */
-
 var secrets = require('./config/secrets');
-var passportConf = require('./config/passport');
 
 /**
  * Create Express server.
  */
-
 var app = express();
-
-/**
- * Connect to MongoDB.
- */
-
-mongoose.connect(secrets.db);
-mongoose.connection.on('error', function() {
-  console.error('MongoDB Connection Error. Please make sure that MongoDB is running.');
-});
-
-/**
- * CSRF whitelist.
- */
-
-var csrfExclude = ['/url1', '/url2'];
 
 /**
  * Express configuration.
  */
-
 app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
-app.set('github_org', 'MozillaFoundation')
-app.set('github_repo', 'plan')
-
+app.set('github_org', 'MozillaFoundation');
+app.set('github_repo', 'plan');
 app.set('view engine', 'jade');
+
+app.use(sessions({
+  cookieName: 'session',
+  secret: secrets.sessionSecret,
+  duration: 24 * 60 * 60 * 1000,
+  activeDuration: 1000 * 60 * 5
+}));
 app.use(compress());
 app.use(connectAssets({
   paths: [path.join(__dirname, 'public/css'), path.join(__dirname, 'public/js')]
 }));
+app.use(express.static(path.join(__dirname, 'public'), { maxAge: 31557600000 }));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(expressValidator());
-app.use(methodOverride());
+app.use(lusca.csrf());
 app.use(cookieParser());
-app.use(session({
-  resave: true,
-  saveUninitialized: true,
-  secret: secrets.sessionSecret,
-  store: new MongoStore({ url: secrets.db, auto_reconnect: true })
-}));
-app.use(passport.initialize());
-app.use(passport.session());
 app.use(flash());
 
+/**
+ * OAuth handler
+ */
+var oauth = require('github-oauth')({
+  githubClient: secrets.github.clientID,
+  githubSecret: secrets.github.clientSecret,
+  baseURL: 'http://localhost:3000',
+  loginURI: '/login',
+  callbackURI: secrets.github.callbackURL,
+  scope: 'user,repo,public_repo'
+});
+
+/**
+ * CORS
+ */
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   next();
 });
 
-app.use(function(req, res, next) {
-  // CSRF protection.
-  if (_.contains(csrfExclude, req.path)) return next();
-  csrf(req, res, next);
-});
 app.use(function(req, res, next) {
   // Make user object available in templates.
   res.locals.user = req.user;
   next();
 });
 
-app.use(express.static(path.join(__dirname, 'public'), { maxAge: 31557600000 }));
-
-app.use(function(req, res, next) {
-  // Remember original destination before login.
-  var path = req.path.split('/')[1];
-  // this next bit is totally weird, needs documentation
-  if (/auth|login|logout|signup|fonts|smalllogo.png|api|now|next|design|tools|mentions|favicon/i.test(path)) {
-    return next();
-  }
-  req.session.returnTo = req.path;
-  next();
-});
+// app.use(function(req, res, next) {
+//   // Remember original destination before login.
+//   var path = req.path.split('/')[1];
+//   // this next bit is totally weird, needs documentation
+//   if (/auth|login|logout|signup|fonts|smalllogo.png|api|now|next|design|tools|mentions|favicon/i.test(path)) {
+//     return next();
+//   }
+//   req.session.returnTo = req.path;
+//   next();
+// });
 
 /**
  * Main routes.
  */
-
 app.get('/', simpleController.index);
 app.get('/now', simpleController.now);
 app.get('/next', simpleController.next);
@@ -134,32 +119,38 @@ app.get('/login', userController.getLogin);
 app.post('/login', userController.postLogin);
 app.get('/logout', userController.logout);
 
-app.get('/intake', passportConf.isAuthenticated, intakeController.getIntake);
-app.post('/intake', intakeController.postIntake);
-app.get('/account', passportConf.isAuthenticated, userController.getAccount);
+// app.get('/intake', passportConf.isAuthenticated, intakeController.getIntake);
+// app.post('/intake', intakeController.postIntake);
+// app.get('/account', passportConf.isAuthenticated, userController.getAccount);
 
-app.post('/account/delete', passportConf.isAuthenticated, userController.postDeleteAccount);
+// app.post('/account/delete', passportConf.isAuthenticated, userController.postDeleteAccount);
 
 app.get('/api/issues', apiController.getIssues)
 app.get('/api/user', apiController.getUser)
 
-app.get('/auth/github', passport.authenticate('github', {scope: 'user,repo,public_repo'}));
-app.get('/auth/github/callback', passport.authenticate('github', {scope: 'user,repo,public_repo', failureRedirect: '/login' }), function(req, res) {
-  res.redirect(req.session.returnTo || '/');
+app.get('/auth/github', oauth.login);
+app.get('/auth/github/callback', function (req, res) {
+  oauth.callback(req, res, function (err, body) {
+    if (err) {
+      req.flash('errors', {msg: err});
+    } else {
+      req.session.token = body.access_token;
+    }
+
+    res.redirect('/');
+  });
 });
 
 /**
  * 500 Error Handler.
  */
-
 app.use(errorHandler());
 
 /**
  * Start Express server.
  */
-
 app.listen(app.get('port'), function() {
-  console.log('Express server listening on port %d in %s mode', app.get('port'), app.get('env'));
+  console.log('Server listening on port %d in %s mode', app.get('port'), app.get('env'));
 });
 
 module.exports = app;
--- controllers/user.js
+++ controllers/user.js
@@ -2,7 +2,6 @@ var _ = require('lodash');
 var async = require('async');
 var crypto = require('crypto');
 var nodemailer = require('nodemailer');
-var passport = require('passport');
 var User = require('../models/User');
 var secrets = require('../config/secrets');
 
@@ -37,18 +36,18 @@ exports.postLogin = function(req, res, next) {
     return res.redirect('/login');
   }
 
-  passport.authenticate('local', function(err, user, info) {
-    if (err) return next(err);
-    if (!user) {
-      req.flash('errors', { msg: info.message });
-      return res.redirect('/login');
-    }
-    req.logIn(user, function(err) {
-      if (err) return next(err);
-      req.flash('success', { msg: 'Success! You are logged in.' });
-      res.redirect(req.session.returnTo || '/');
-    });
-  })(req, res, next);
+  // passport.authenticate('local', function(err, user, info) {
+  //   if (err) return next(err);
+  //   if (!user) {
+  //     req.flash('errors', { msg: info.message });
+  //     return res.redirect('/login');
+  //   }
+  //   req.logIn(user, function(err) {
+  //     if (err) return next(err);
+  //     req.flash('success', { msg: 'Success! You are logged in.' });
+  //     res.redirect(req.session.returnTo || '/');
+  //   });
+  // })(req, res, next);
 };
 
 /**
